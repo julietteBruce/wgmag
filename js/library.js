6c8b99f2a6f3e685578965d4fb7101c3f7b8e2c9
Juliette Bruce <jemmy.bruce@gmail.com>
2020-04-30 15:34:36 -0500

    js for dirrectory

--- js/library.js
+++ js/library.js
@@ -17,6 +17,33 @@ function init_document_ready() {
 
 }
 
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
 // - - - - - - - - - - - - - - - - - - - - - - - - -
 // for each li.color, grab the hex color value in the item and set is at the background-color 
 
